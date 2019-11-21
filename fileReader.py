9f70dc22adedf42c135577d5be32a47f8c10bee1
dfeldan <dfeldan@mail.yu.edu>
2019-11-20 21:27:07 -0500

    Cleaned up FileReader for public use

--- fileReader.py
+++ fileReader.py
@@ -1,22 +1,23 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+
 G = nx.Graph()
-#G.add_node("Node 1")
-
-
 
 file = open("Network.txt", "rb")
 G = nx.read_weighted_edgelist(file)
 file.close()
 
+#List the nodes
 #print(list(G.nodes))
 
-print(G.number_of_nodes())
-print(G.number_of_edges())
+#Get Number of nodes and edges
+print(G.number_of_nodes()) #Should be 37
+print(G.number_of_edges())	#SHould be 78
+
+#Attempt to graph the network
 #plt.subplot(121)
 #nx.draw(G, with_labels=True, font_weight='bold')
 #plt.subplot(122)
 #nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
-#text = file.readlines()
-#file.close()
+
 
